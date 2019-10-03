aec6848d37d384a9bae6d21f77af8a147463618f
Michel A. Cuendet <mac2109@med.cornell.edu>
2019-10-03 17:40:07 -0400

    Added SLURM support

--- RUN.sh
+++ RUN.sh
@@ -217,39 +217,46 @@ cd ..
 
 echo "Submitting all sub-jobs ... "
 
-# SGE : 
+# SGE :  ----------------------------------------------------------
 if [ $queueing_system == "SGE" ]; then 
+
 	jobid_raw=$( qsub -v mmgbsa_path=$mmgbsa_path -v queueing_system=$queueing_system $res_req -q $queue_name -t 1-$n_jobs -tc $max_jobs_running_simultaneously  $parallel_scripts/mmgbsa_master_submit.sh $traj $start_frame $frame_stride $frames_per_job )
 	# The -v option to qsub pushes the  environment variables from the shell executing qsub
 	# This is needed to pass the mmgbsa_path global variable. 
 	jobid=$( echo $jobid_raw | awk '{split($3,jjj,"."); print jjj[1]}' )
-fi
 
-# LSF :
-if [ $queueing_system == "LSF" ]; then
+        # Post-processing job
+        echo "Submitting final post-processing job ... "
+        qsub -v mmgbsa_path=$mmgbsa_path -v queueing_system=$queueing_system $res_req -q $queue_name -hold_jid $jobid $parallel_scripts/mmgbsa_final_submit.sh $traj $n_jobs $frames_per_job
+        qstat
+
+# LSF :  ----------------------------------------------------------
+elif [ $queueing_system == "LSF" ]; then
+
 	jobid_raw=$( bsub -q $queue_name -n 1 -o "mmgbsa2.1.o%J.%I" -e "mmgbsa2.1.e%J.%I"  -J "mmgbsa.[1-$n_jobs]"  " sh $parallel_scripts/mmgbsa_master_submit.sh $traj $start_frame $frame_stride $frames_per_job" )
 	echo $jobid_raw
 	jobid=$( echo $jobid_raw  | awk '{print substr($2,2,length($2)-2)}' )
 	# In LSF, (almost) all environment variables are passed by default.
 	# Passing arguments is not as easy, and requires the sub-sh construct. 
-fi
 
-###################################################################
-# Submit post-processing job
+        # Post-processing job
+        echo "Submitting final post-processing job ... "
+        bsub -q $queue_name -n 1 -o "mmgbsa2.1_final.o%J.%I" -e "mmgbsa2.1_final.e%J.%I" -w "done($jobid)" -J "mmgbsa_final" " sh $parallel_scripts/mmgbsa_final_submit.sh $traj $n_jobs $frames_per_job "
+        # Passing arguments is not as easy, and requires the sub-sh construct. 
+        bjobs
 
-echo "Submitting final post-processing job ... "
+# SLURM :  ----------------------------------------------------------
+elif [ $queueing_system == "SLURM" ]; then
 
-# SGE : 
-if [ $queueing_system == "SGE" ]; then
-	qsub -v mmgbsa_path=$mmgbsa_path -v queueing_system=$queueing_system $res_req -q $queue_name -hold_jid $jobid $parallel_scripts/mmgbsa_final_submit.sh $traj $n_jobs $frames_per_job 
- 	qstat
-fi
+	jobid=`sbatch --array=1-${n_jobs}%${max_jobs_running_simultaneously} -p $queue_name $parallel_scripts/mmgbsa_master_submit_slurm.sh $traj $start_frame $frame_stride $frames_per_job | egrep -o -e "\b[0-9]+$"`
 
-# LSF :
-if [ $queueing_system == "LSF" ]; then
-	bsub -q $queue_name -n 1 -o "mmgbsa2.1_final.o%J.%I" -e "mmgbsa2.1_final.e%J.%I" -w "done($jobid)" -J "mmgbsa_final" " sh $parallel_scripts/mmgbsa_final_submit.sh $traj $n_jobs $frames_per_job "
-	# Passing arguments is not as easy, and requires the sub-sh construct. 
-	bjobs
+        # Submit post-processing job
+	echo "Submitting final post-processing job ... "
+	sbatch --dependency=afterok:${jobid} -p $queue_name $parallel_scripts/mmgbsa_final_submit_slurm.sh $traj $n_jobs $frames_per_job
+	squeue -u `whoami`
+else
+        echo "ERROR: bad queuing system identifyiner"
+	exit 1
 fi
 
 exit
